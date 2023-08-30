7e9c93945ca71a7fb02dd82fcf7b11a5120e4976
PerdomoPer <gustavo.perdomo@estudante.ifms.edu.br>
2023-08-30 07:45:44 -0400

    tabaio

--- src/componentes/main/Main.tsx
+++ src/componentes/main/Main.tsx
@@ -6,7 +6,8 @@ function Main(){
         <div >
         <img className='main-foto' src={imagem} alt="" /> 
         <h1 className='main-h'>Olá, Eu me chamo Gustavo Ferreira Perdomo</h1>
-        <p className='main-p'>Eu sou aluno formado no curso tecnico em informatica pelo ifms campus navirai, fui medalhista na oliempiada brasileira de matematica, com experiencia em comercio de produtos escolares, brinquedos e moveis.</p>
+        <p className='main-p'>Eu sou aluno formado no curso tecnico em informatica pelo ifms campus navirai, fui medalhista na olimpiada 
+        brasileira de matematica, com experiencia em comercio de produtos escolares, brinquedos e moveis.</p>
         </div>
         
     )
